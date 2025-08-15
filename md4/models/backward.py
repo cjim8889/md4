fix: Update embedding and normalization dtypes to use instance-specific dtype for consistency and flexibility

md4/models/backward.py
@@ -93,11 +93,11 @@ class DiscreteClassifier(nn.Module):
             # z: [bs, seq_len] or [bs, h, w, c]
             assert jnp.isscalar(t) or t.ndim == 0 or t.ndim == 1
             t = t * jnp.ones(z.shape[0])  # ensure t is a vector
-            cond = CondEmbedding(self.feature_dim, dtype=jnp.float32, param_dtype=self.param_dtype)(t * 1000, cond=cond)
+            cond = CondEmbedding(self.feature_dim, dtype=self.dtype, param_dtype=self.param_dtype)(t * 1000, cond=cond)
 
         if z.ndim == 2:
             if self.outside_embed:
-                z = nn.Embed(self.vocab_size + 1, self.feature_dim, dtype=jnp.float32, param_dtype=self.param_dtype)(z)
+                z = nn.Embed(self.vocab_size + 1, self.feature_dim, dtype=self.dtype, param_dtype=self.param_dtype)(z)
             if self.model_sharding:
                 args = sharded_transformer.ModelArgs(
                     dim=self.feature_dim * self.num_heads,

md4/models/diffusion/md4.py
@@ -118,7 +118,7 @@ class MD4(nn.Module):
         self.noise_schedule = MaskingSchedule(self.data_shape, self.noise_schedule_type)
 
         if self.classes > 0:
-            self.cond_embeddings = nn.Embed(self.classes, self.feature_dim, dtype=jnp.float32, param_dtype=self.param_dtype)
+            self.cond_embeddings = nn.Embed(self.classes, self.feature_dim, dtype=self.dtype, param_dtype=self.param_dtype)
         if self.fingerprint_dim > 0:
             if self.fingerprint_adapter:
                 self.fp_adapter = FingerprintAdapter(
@@ -140,7 +140,7 @@ class MD4(nn.Module):
                 param_dtype=self.param_dtype,
             )
         if self.atom_type_size > 0:
-            self.atom_embeddings = nn.Embed(self.atom_type_size, self.feature_dim, dtype=jnp.float32, param_dtype=self.param_dtype)
+            self.atom_embeddings = nn.Embed(self.atom_type_size, self.feature_dim, dtype=self.dtype, param_dtype=self.param_dtype)
             self.atom_embeddings_agg = nn.Dense(features=self.feature_dim, name="atom_embeddings_agg", dtype=self.dtype, param_dtype=self.param_dtype)
 
         self.classifier = backward.DiscreteClassifier(
@@ -221,7 +221,7 @@ class MD4(nn.Module):
                 if "atom_types" in conditioning:
                     atom_conditioning = self.atom_embeddings(conditioning["atom_types"])
                     atom_conditioning = jax.vmap(self.atom_embeddings_agg)(atom_conditioning)
-                    atom_conditioning = nn.swish(atom_conditioning.astype(jnp.float32)).astype(self.dtype)
+                    atom_conditioning = nn.swish(atom_conditioning.astype(self.dtype))
 
                     if atom_conditioning.ndim == 2:
                         atom_conditioning = jnp.sum(atom_conditioning, axis=0)

md4/networks/transformer.py
@@ -336,10 +336,10 @@ class TransformerBlock(nn.Module):
                 jnp.split(ln(cond)[:, None, :], 6, axis=-1)
             )
             attention_norm = nn.LayerNorm(
-                epsilon=self.args.norm_eps, use_bias=False, use_scale=False, dtype=jnp.float32, param_dtype=jnp.float32
+                epsilon=self.args.norm_eps, use_bias=False, use_scale=False, dtype=self.args.dtype, param_dtype=self.args.param_dtype
             )
             ffn_norm = nn.LayerNorm(
-                epsilon=self.args.norm_eps, use_bias=False, use_scale=False, dtype=jnp.float32, param_dtype=jnp.float32
+                epsilon=self.args.norm_eps, use_bias=False, use_scale=False, dtype=self.args.dtype, param_dtype=self.args.param_dtype
             )
             h = x + gate_att * self.attention(
                 attention_norm(x) * (scale_att + 1.0) + shift_att,
@@ -351,8 +351,8 @@ class TransformerBlock(nn.Module):
                 ffn_norm(h) * (scale_mlp + 1.0) + shift_mlp, train=train
             )
         else:
-            attention_norm = RMSNorm(self.args.dim, eps=self.args.norm_eps, dtype=jnp.float32, param_dtype=jnp.float32)
-            ffn_norm = RMSNorm(self.args.dim, eps=self.args.norm_eps, dtype=jnp.float32, param_dtype=jnp.float32)
+            attention_norm = RMSNorm(self.args.dim, eps=self.args.norm_eps, dtype=self.args.dtype, param_dtype=self.args.param_dtype)
+            ffn_norm = RMSNorm(self.args.dim, eps=self.args.norm_eps, dtype=self.args.dtype, param_dtype=self.args.param_dtype)
             h = x + self.attention(attention_norm(x), freqs_cos, freqs_sin, train=train)
             out = h + self.feed_forward(ffn_norm(h), train=train)
 
@@ -369,7 +369,7 @@ class Transformer(nn.Module):
             output_channels = args.output_channels
 
         if args.embed_input:
-            h = nn.Embed(args.n_embed_classes, args.dim, dtype=jnp.float32, param_dtype=args.param_dtype)(x)
+            h = nn.Embed(args.n_embed_classes, args.dim, dtype=args.dtype, param_dtype=args.param_dtype)(x)
             if args.dropout_rate > 0.0:
                 h = nn.Dropout(args.dropout_rate, deterministic=not train)(h)
         else:
@@ -388,7 +388,7 @@ class Transformer(nn.Module):
 
         if cond is not None:
             output_norm = nn.LayerNorm(
-                epsilon=args.norm_eps, use_bias=False, use_scale=False, dtype=jnp.float32, param_dtype=jnp.float32
+                epsilon=args.norm_eps, use_bias=False, use_scale=False, dtype=args.dtype, param_dtype=args.param_dtype
             )
             activation = activation_map[args.mlp_type]
             if args.cond_type == "adaln":
@@ -409,7 +409,7 @@ class Transformer(nn.Module):
                             use_bias=True,
                             kernel_init=nn.initializers.zeros,
                             bias_init=nn.initializers.zeros,
-                            dtype=jnp.float32,
+                            dtype=jnp.dtype,
                             param_dtype=jnp.float32,
                         ),
                     ]
@@ -421,7 +421,7 @@ class Transformer(nn.Module):
                 output_channels, use_bias=False, kernel_init=nn.initializers.zeros, dtype=jnp.float32, param_dtype=jnp.float32
             )(output_norm(h) * (scale_out + 1) + shift_out)
         else:
-            h = RMSNorm(args.dim, args.norm_eps, dtype=jnp.float32, param_dtype=jnp.float32)(h)
+            h = RMSNorm(args.dim, args.norm_eps, dtype=args.dtype, param_dtype=args.param_dtype)(h)
             logits = nn.Dense(
                 features=output_channels,
                 use_bias=False,