f89c6556e56c891b6cf4ed82e19902e750501faf
Wccccc <43540548+cjim8889@users.noreply.github.com>
2025-08-22 15:01:30 +0100

    refactor: Update num_microbatches from 12 to 6 for optimized batch processing

--- md4/configs/md4/molecular_xtra_large_sp_bf16_multi_host.py
+++ md4/configs/md4/molecular_xtra_large_sp_bf16_multi_host.py
@@ -92,7 +92,7 @@ def get_config() -> config_dict.ConfigDict:
     # Evaluates for a full epoch if num_eval_steps==-1.
     config.num_eval_steps = 1000
     config.batch_size = 960
-    config.num_microbatches = 12
+    config.num_microbatches = 6
     config.per_device_batch_size = -1
     # If batches should be added to evaluate the entire dataset.
     config.eval_pad_last_batch = False
